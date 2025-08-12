f14db1ed2246977e617030b4f862ed47726476bc
Thaliios <cahuzac.p@gmail.com>
2025-08-12 22:04:55 +0200

    CORS WIP

--- api/server.js
+++ api/server.js
@@ -43,8 +43,8 @@ const corsOptions = {
 
 
 // CORS et gestion du préflight
-//app.use(cors(corsOptions));
-app.use(cors()); 
+app.use(cors(corsOptions));
+//app.use(cors()); 
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
