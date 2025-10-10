791beb8be465915e41452e55fc1c01d285871cb1
Thaliios <cahuzac.p@gmail.com>
2025-10-10 10:41:22 +0200

    wip

--- app/src/components/ui/itemComponent.tsx
+++ app/src/components/ui/itemComponent.tsx
@@ -1,4 +1,5 @@
 import ItemProps from "../../@interface/ItemProps";
+import NoImageAvailable from "/public/img/no_image_available.svg";
 
 
 const ItemComponent = ({
@@ -10,7 +11,6 @@ const ItemComponent = ({
 
 }) => {
 
-
     return (
         <article
             key={item.id}
@@ -18,33 +18,30 @@ const ItemComponent = ({
             className="homepage__item"                            >
             <div className="homepage__item__image-wrapper">
                 <img
-                    src={item.images && item.images.length > 0
+                    src={item?.images[0]?.url
                         ?
                         `${item.images[0].url}`
                         :
-                        item.cover || "/path/to/default/image.jpg"}
-                      
+                        NoImageAvailable
+                    }
                     className="homepage__item__image"
                     loading="lazy"
                     alt={`Image de l'objet ${item?.name.toLowerCase() || 'l\'item'}`}
-
-                    
                 />
                 <div className="homepage__item__date">
                     Ajouté le : {item.createdAt ? new Date(item.createdAt).toLocaleDateString() : 'Date inconnue'}
                 </div>
-                <div className="homepage__item__creator">
-                   Crée par : {item.creator.username && item.creator.username == "SystemDeleted" ? "Utilisateur supprimé" : item.creator.username }
-                </div>
             </div>
             <div className="homepage__item__content">
-                <div className="homepage__item__title">{item.name}</div>
+                <h1 className="homepage__item__title">{item.name}</h1>
                 <div className="homepage__item__details">
                     <div className="homepage__item__description">
                         {item.description}
                     </div>
+                    <div className="homepage__item__creator">
+                        Ajouté par : {item.creator.username && item.creator.username == "SystemDeleted" ? "Utilisateur supprimé" : item.creator.username}
+                    </div>
                 </div>
-
             </div>
         </article>
     )
